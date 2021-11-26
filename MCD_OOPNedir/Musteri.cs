9c18cbdca77dc049f71c52a8595ff99836d41e92
Esra Alkan <es.alknn@gmail.com>
2021-11-26 20:07:02 +0300

    OOPOrnek

---
+++ MCD_OOPNedir/Musteri.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPNedir
+{
+    class Musteri
+    {
+        /*
+         * TcKimlik Numarası
+         * İsim
+         * Soyisim
+         * Cinsiyet
+         * 
+         */
+
+        public string TcKimlikNo;
+        public string Isim;
+        public string SoyIsim;
+        public int Cinsiyet;//717770001:Erkek , 717770005:Kadın
+
+        public Musteri() { }
+
+        public Musteri(string _tcno,string _isim)
+        {
+            TcKimlikNo = _tcno;
+            Isim = _isim;
+           
+        }
+
+      public bool MusteriKontrol()
+        {
+            bool kontrol = MusteriKontrolDataBase(TcKimlikNo);//Metot içinde metot çağırdık!
+            return true;
+        }
+
+        private bool MusteriKontrolDataBase(string Tckimliknumarasi)
+        {
+            //DateBase gidilir müşterinin tckimlik numarasına göre kayıt edilip edilmediği bilgisi sorgulanır.....
+            return true;
+        }
+
+    }
+}
--- MCD_OOPNedir/Program.cs
+++ MCD_OOPNedir/Program.cs
@@ -13,15 +13,39 @@ namespace MCD_OOPNedir
             //Arac bmw = new Arac("bmw","jeep");
             //Arac mercedes = new Arac();
             #region Araç Sınıfı İnceleme
-            Arac A1 = new Arac("Opel", "Corsa", 2010, 56000);
-            A1.alisFiyat = 28000;
-            A1.satisFiyat = 32000;
-            A1.MaxIndirimTutar = 1500;
-            //A1.Fiyat = Kapsülleme konusu
-            A1.FiyatAta(31000);
-            A1.Fiyat = 35000;
-            Console.WriteLine(A1.Fiyat);
-            A1.BilgileriGoruntule();
+            //Arac A1 = new Arac("Opel", "Corsa", 2010, 56000);
+            //A1.alisFiyat = 28000;
+            //A1.satisFiyat = 32000;
+            //A1.MaxIndirimTutar = 1500;
+            ////A1.Fiyat = Kapsülleme konusu
+            //A1.FiyatAta(31000);
+            //A1.Fiyat = 35000;
+            //Console.WriteLine(A1.Fiyat);
+            //A1.BilgileriGoruntule();
+            #endregion
+
+            #region Musteri Sınıfı İnceleme
+            Musteri M1 = new Musteri("122544013254", "Murat");
+            M1.TcKimlikNo = "1225440152";
+            M1.Isim = "Cengiz";
+            M1.SoyIsim = "Atilla";
+            M1.Cinsiyet = 717770001;
+
+            Musteri M2 = M1;
+            M2.Isim = "Murat";
+            M2.TcKimlikNo = "12254401325";
+
+            bool musteriKontrol = M2.MusteriKontrol();
+
+            Console.WriteLine(musteriKontrol);
+
+            M2 = null;
+            M1 = null;
+
+            string isim = M2.Isim;
+
+            //Customer M3 = new Customer ();
+
             #endregion
 
             Console.ReadLine();
